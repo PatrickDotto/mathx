47031e45dcb459fe4f542991b0414abfebceda69
Patrick Dotto <patrickdotto05@gmail.com>
2026-02-13 15:08:10 -0300

    Criação do Controller, métodos e rotas.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'mathx';
-});
+Route::get('/', [App\Http\Controllers\MainController::class, 'home'])->name('home');
+Route::post('/generate-exercises', [App\Http\Controllers\MainController::class, 'generateExercises'])->name('generate-exercises');
+Route::get('/print-exercises', [App\Http\Controllers\MainController::class, 'printexercises'])->name('print-exercises');
+Route::get('/export-exercises', [App\Http\Controllers\MainController::class, 'exportExercises'])->name('export-exercises');
